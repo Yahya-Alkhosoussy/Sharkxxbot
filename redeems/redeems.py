f54501fee6ce8d9cf022c41f8d9975ca9685e4d4
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-06-13 13:31:27 +0100

    Added random chocies for the shark tooth redemption

--- redeems/redeems.py
+++ redeems/redeems.py
@@ -1,13 +1,20 @@
-from redeems.SQL.shark_tooth import add_shark_tooth, add_user_to_shark_tooth, is_user_in_db
+from random import choice
+
+from redeems.SQL.shark_tooth import add_shark_tooth, add_user_to_shark_tooth, get_crystal_list, is_user_in_db
 from redeems.SQL.VIP import add_user_to_VIP
 
 
 async def deal_with_sharktooth(username: str, user_id: int):
     is_in_db = await is_user_in_db(user_id)
+
+    crystal_list = await get_crystal_list()
+
+    crystal = choice(list(crystal_list))[0]
+
     if is_in_db:
-        await add_shark_tooth(username, user_id)
+        await add_shark_tooth(username, user_id, crystal)
     else:
-        await add_user_to_shark_tooth(username, user_id)
+        await add_user_to_shark_tooth(username, user_id, crystal)
 
 
 async def deal_with_VIP(username: str, user_id: int):
