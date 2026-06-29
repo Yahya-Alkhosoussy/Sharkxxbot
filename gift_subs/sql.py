fec5ecdbc84dbc39c242a27f57be85a971b77d80
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-06-29 03:46:11 +0100

    Added a way to fetch rewards

--- gift_subs/sql.py
+++ gift_subs/sql.py
@@ -90,3 +90,13 @@ async def get_gifted_count(user: TwitchUser) -> int:
             if result is None:
                 raise ValueError("Could not find gift count")
             return result[0]
+
+
+async def get_rewards() -> dict[str, int]:
+    async with connect(db_path) as conn:
+        async with conn.execute("SELECT reward, sub_count FROM rewards") as cur:
+            results = await cur.fetchall()
+            returns: dict[str, int] = {}
+            for result in results:
+                returns[result[0]] = result[1]
+            return returns
